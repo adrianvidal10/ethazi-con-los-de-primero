ed583dd608b2e55299a58250722e8999d0336c50
goizane <goizane.terrerosor@elorrieta-errekamari.com>
2019-04-08 13:25:34 +0200

    Erreserbak egiteko prest, erreserba egiteko plazak dauden begiratzen du, eta hasiera datak now()ren ondoren dira

--- src/vista/ErreserbaEgin.java
+++ src/vista/ErreserbaEgin.java
@@ -1,6 +1,7 @@
 package vista;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -14,10 +15,23 @@ import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 import com.toedter.calendar.JCalendar;
 import com.toedter.calendar.JDateChooser;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.util.Date;
+import java.awt.event.ActionEvent;
 
 public class ErreserbaEgin extends JFrame {
 
 	private JPanel contentPane;
+	JDateChooser dateChooser, dateChooser_1;
+	JLabel lblHasieraData, lblAmaieraData, lblErreserbaEgin, lblOstatuarenIzena, lblPrezioTotala, lblOheMota,
+			lblOstatuarenIzenaErakutsi, lblGelaMota, lblPrezioTotErakutsi;
+
+	JButton btnAtzera, btnOrdainketaBurutu;
+	JComboBox cbxOheMota, cbxGelaMota;
+
+	Date gaur = new Date();
+	private JLabel lblDisponibilidad;
 
 	/**
 	 * Launch the application.
@@ -45,54 +59,99 @@ public class ErreserbaEgin extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JButton btnAtzera = new JButton("Atzera");
+
+		btnAtzera = new JButton("Atzera");
 		btnAtzera.setBounds(10, 11, 65, 23);
 		contentPane.add(btnAtzera);
-		
-		JLabel lblErreserbaEgin = new JLabel("Erreserba egin");
+
+		lblErreserbaEgin = new JLabel("Erreserba egin");
 		lblErreserbaEgin.setFont(new Font("Times New Roman", Font.PLAIN, 25));
 		lblErreserbaEgin.setBounds(144, 11, 143, 29);
 		contentPane.add(lblErreserbaEgin);
-		
-		JLabel lblOstatuarenIzena = new JLabel("Izena:");
-		lblOstatuarenIzena.setBounds(28, 70, 47, 14);
+
+		lblOstatuarenIzena = new JLabel("Izena:");
+		lblOstatuarenIzena.setBounds(25, 80, 47, 14);
 		contentPane.add(lblOstatuarenIzena);
-		
-		JLabel lblOheMota = new JLabel("Ohe mota:");
-		lblOheMota.setBounds(28, 150, 65, 14);
+
+		lblOheMota = new JLabel("Ohe mota:");
+		lblOheMota.setBounds(25, 158, 65, 14);
 		contentPane.add(lblOheMota);
-		
-		JLabel lblOstatuarenIzenaErakutsi = new JLabel("New label");
-		lblOstatuarenIzenaErakutsi.setBounds(70, 70, 111, 14);
+
+		lblOstatuarenIzenaErakutsi = new JLabel("New label");
+		lblOstatuarenIzenaErakutsi.setBounds(67, 80, 111, 14);
 		contentPane.add(lblOstatuarenIzenaErakutsi);
-		
-		JLabel lblGelaMota = new JLabel("Gela mota:");
-		lblGelaMota.setBounds(28, 108, 65, 14);
+
+		lblGelaMota = new JLabel("Gela mota:");
+		lblGelaMota.setBounds(25, 122, 65, 14);
 		contentPane.add(lblGelaMota);
-		
-		JComboBox cbxGelaMota = new JComboBox();
-		cbxGelaMota.setBounds(89, 144, 92, 20);
+
+		cbxGelaMota = new JComboBox();
+		cbxGelaMota.setBounds(100, 155, 92, 20);
 		contentPane.add(cbxGelaMota);
-		
-		JComboBox cbxOheMota = new JComboBox();
-		cbxOheMota.setBounds(89, 105, 92, 20);
+
+		cbxOheMota = new JComboBox();
+		cbxOheMota.setBounds(100, 119, 92, 20);
 		contentPane.add(cbxOheMota);
-		
-		JButton btnOrdainketaBurutu = new JButton("Ordainketa burutu");
+
+		btnOrdainketaBurutu = new JButton("Ordainketa burutu");
 		btnOrdainketaBurutu.setBounds(276, 224, 132, 23);
 		contentPane.add(btnOrdainketaBurutu);
-		
-		JLabel lblPrezioTotala = new JLabel("Prezio totala:");
+
+		lblPrezioTotala = new JLabel("Prezio totala:");
 		lblPrezioTotala.setBounds(28, 228, 70, 14);
 		contentPane.add(lblPrezioTotala);
-		
-		JLabel lblPrezioTotErakutsi = new JLabel("New label");
+
+		lblPrezioTotErakutsi = new JLabel("New label");
 		lblPrezioTotErakutsi.setBounds(108, 228, 73, 14);
 		contentPane.add(lblPrezioTotErakutsi);
-		
-		JDateChooser dateChooser = new JDateChooser();
-		dateChooser.setBounds(313, 58, 95, 20);
+
+		dateChooser = new JDateChooser();
+		dateChooser.setMinSelectableDate(gaur);
+		dateChooser.setBounds(301, 80, 106, 20);
 		contentPane.add(dateChooser);
+
+		dateChooser_1 = new JDateChooser();
+
+		Date erreserbaHasiera = dateChooser.getDate();
+
+		dateChooser_1.getCalendarButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				dateChooser_1.setMinSelectableDate(gaur);	
+				//dateChooser_1.setMinSelectableDate(erreserbaHasiera);
+			}
+		});
+
+		dateChooser_1.setBounds(301, 122, 105, 20);
+		contentPane.add(dateChooser_1);
+
+		lblHasieraData = new JLabel("Hasiera data:");
+		lblHasieraData.setBounds(219, 80, 82, 14);
+		contentPane.add(lblHasieraData);
+
+		lblAmaieraData = new JLabel("Amaiera data:");
+		lblAmaieraData.setBounds(219, 122, 75, 14);
+		contentPane.add(lblAmaieraData);
+		
+		lblDisponibilidad = new JLabel("New label");
+		lblDisponibilidad.setBounds(232, 172, 165, 14);
+		
+		int cont = 1;
+		
+		if (cont == 0) {
+			
+			lblDisponibilidad.setText("Ez daude plazarik erreserbak egiteko");
+		lblDisponibilidad.setForeground(Color.red);
+			btnOrdainketaBurutu.setVisible(false);
+			
+		} else {
+			
+			lblDisponibilidad.setText("Plazak eskuragarriak");
+			lblDisponibilidad.setForeground(Color.green);
+				btnOrdainketaBurutu.setVisible(true);
+		}
+		
+		contentPane.add(lblDisponibilidad);
+		
 	}
 }
